9b479fe65e29e8ef86e198e17ee8b2be6321051a
yoranhrv <yoran.horvaispro@gmail.com>
2026-09-22 11:17:12 +0200

    Update HelloWorld to include GitHub message

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World by YH!");
+        System.out.println("Hello World by YH on github!");
     }
     
 }
