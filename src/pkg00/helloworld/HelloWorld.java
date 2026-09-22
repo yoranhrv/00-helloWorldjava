7f8448db7672571411557eff32daeb53b2c4e1d8
yhorvais <yhorvais@350028R-F115X15.lise.kreizenn.bzh>
2026-09-22 12:16:59 +0200

    Code qui ache un message dans la console sur plusieurs lignes.

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -15,6 +15,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World by YH on github!");
+        System.out.println("Ligne 2");
     }
     
 }
