36f27c2d22f87bd2c588998c29622a4d7c304c61
yhorvais <yhorvais@350028R-F115X15.lise.kreizenn.bzh>
2026-09-25 14:51:51 +0200

    Exercice 4.

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +15,30 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
+        Ex2:
         System.out.println("Hello World by YH on github!");
         System.out.println("Ligne 2");
+        
+        Ex4*/
+    Scanner sc = new Scanner(System.in);
+    
+        System.out.print("x : ");
+        int x = sc.nextInt();
+    
+        System.out.print("y : ");
+        int y = sc.nextInt();
+
+        System.out.println("Avant l'echange : x = " + x + ", y = " +y );
+
+        int z = 0;
+        
+        z = x;
+        x = y;
+        y = z;
+
+        System.out.println("Apres l'echange : x = " + x + ", y = " + y);
+           
     }
     
 }
